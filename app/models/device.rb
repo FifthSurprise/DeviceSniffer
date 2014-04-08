279adbf98ec3741b3a81b6f0cb9dff146028c62f
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-08 17:23:38 -0400

    Testing

--- app/models/device.rb
+++ app/models/device.rb
@@ -6,6 +6,10 @@ class Device < ActiveRecord::Base
   def manufacturer
     uri = URI.parse("http://www.macvendorlookup.com/api/v2/#{self.macaddress}")
     response = Net::HTTP.get_response(uri)
-    JSON.parse(response.body).first["company"]
+    if response.body.length==0
+      return ""
+    else
+      JSON.parse(response.body).first["company"]
+    end
   end
 end
