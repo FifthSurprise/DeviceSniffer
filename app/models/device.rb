062983df393b790af974bfb2155beccbde7e3f57
prashmukho <prashmukho@github.com>
2014-04-08 16:57:43 -0400

    added manufacturer method

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,11 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
 class Device < ActiveRecord::Base
+  def manufacturer
+    uri = URI.parse("http://www.macvendorlookup.com/api/v2/#{self.macaddress}")
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body).first["company"]
+  end
 end
