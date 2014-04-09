6e63423dec2e714110edbcd07d912b6725ed7d08
prashmukho <prashmukho@github.com>
2014-04-09 11:43:13 -0400

    added naming code

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ end
 
 gem "bootstrap-sass", "~> 3.1.1"
 gem "devise"
+gem "faker"
--- app/models/device.rb
+++ app/models/device.rb
@@ -8,4 +8,23 @@ class Device < ActiveRecord::Base
     response = Net::HTTP.get_response(uri)
     response.body == "" ? "" : JSON.parse(response.body).first["company"]
   end
+  
+  def self.generate_names(num)
+    animals = File.readlines("lib/naming/animals.txt")
+    adjectives = File.readlines("lib/naming/adjectives.txt")
+    
+    File.open("lib/naming/names.txt", "w") do |file|
+      num.times do 
+        to_add = adjectives.sample.strip + "-" + animals.sample.strip + "-"+ "#{rand(10..99)}" +"\n"
+        file.write(to_add)
+      end
+    end
+  end
+  
+  def self.get_name
+    self.generate_names(100000) unless File.readlines("lib/naming/names.txt").count > 0
+    
+    File.readlines("lib/naming/names.txt").sample.strip
+  end
 end
+
