6aac9b17bac29226788ef3e0f745d0e57e06299a
Jisu Kim <jisu.kim@flatironschool.com>
2014-04-09 14:20:06 -0400

    Update Dashboard

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -1,7 +1,7 @@
 require 'uri'
 require 'net/http'
 require 'json'
-require_relative '../models/device.rb'
+# require_relative '../models/device.rb'
 
 current_valuation = 0
 
@@ -15,6 +15,6 @@ Dashing.scheduler.every '5s' do
   
   #top_manufacturers  = [{:label=>"Apple", :value=>50}, { :label=>"HTC", :value=>20}, {:label=>'Samsung', :value=>22}]
   top_manufacturers=[]
-  top_manufacturers<<Dashing.companies[0..2]
+  top_manufacturers<<Device.manufacturers_dashboard
   Dashing.send_event('top3manufacturers', {items: top_manufacturers})
 end
