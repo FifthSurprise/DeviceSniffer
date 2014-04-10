0b1bd9a4009badc58a5f0a7abc87fdfd042d9f94
Jisu Kim <jisu.kim@flatironschool.com>
2014-04-10 16:41:11 -0400

    Change sightings per hr to foot traffic and to a meter

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -5,13 +5,13 @@ require 'json'
 
 current_valuation = 0
 
-Dashing.scheduler.every '3s' do
+Dashing.scheduler.every '300s' do
   last_valuation = current_valuation
   current_valuation = rand(100)
 
   Dashing.send_event('valuation', { current: current_valuation, last: last_valuation })
   Dashing.send_event('macaddresses',   { current: Device.total_Count })
-  Dashing.send_event('past_hour',   { current: Device.sightings_past_hour })
+  Dashing.send_event('past_hour',   { value: Device.sightings_past_hour })
   Dashing.send_event('past_day',   { current: Device.sightings_past_day })
 
   #top_manufacturers  = [{:label=>"Apple", :value=>50}, { :label=>"HTC", :value=>20}, {:label=>'Samsung', :value=>22}]
