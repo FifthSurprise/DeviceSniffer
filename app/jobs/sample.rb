404984f8ae82a920fccaedc1a12155dbf4a49d14
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-12 11:47:25 -0400

    update scheduled tasks to reduce connections

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -1,28 +1,26 @@
 require 'uri'
 require 'net/http'
 require 'json'
-# require_relative '../models/device.rb'
-
-current_valuation = 0
 
 Dashing.scheduler.every '10s' do
-  last_valuation = current_valuation
-  current_valuation = rand(100)
-
-  Dashing.send_event('valuation', { current: current_valuation, last: last_valuation })
-  Dashing.send_event('macaddresses',   { current: Device.total_Count })
   Dashing.send_event('past_hour',   { value: Device.sightings_past_hour })
-  Dashing.send_event('past_day',   { current: Device.sightings_past_day })
+
   d = Device.where("company != ''").last
   company = d.company !="" ? d.company : "Device Manufacturer Not Found"
   Dashing.send_event('last_MAC', { text: "#{company}",
                                    moreinfo: "MAC Address: #{d.macaddress}"})
-  #top_manufacturers  = [{:label=>"Apple", :value=>50}, { :label=>"HTC", :value=>20}, {:label=>'Samsung', :value=>22}]
-  # top_manufacturers=[]
   top_manufacturers=Device.manufacturers_dashboard
-  Dashing.send_event('manufacturers', {items: top_manufacturers})
 end
-Dashing.scheduler.every '30s' do
-  Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
 
+Dashing.scheduler.every '5s' do
+  Dashing.send_event('macaddresses',   { current: Device.total_Count })
+end
+
+Dashing.scheduler.every '60s' do
+  Dashing.send_event('past_day',   { current: Device.sightings_past_day })
+end
+
+Dashing.scheduler.every '30s' do
+  Dashing.send_event('manufacturers', {items: top_manufacturers})
+  Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
 end
