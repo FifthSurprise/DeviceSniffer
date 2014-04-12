c866bf922d99a20141755dc40c749ecf5848bf8f
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-12 12:10:03 -0400

    Fixed missing line in Dashing scheduler

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -9,7 +9,7 @@ Dashing.scheduler.every '10s' do
   company = d.company !="" ? d.company : "Device Manufacturer Not Found"
   Dashing.send_event('last_MAC', { text: "#{company}",
                                    moreinfo: "MAC Address: #{d.macaddress}"})
-  top_manufacturers=Device.manufacturers_dashboard
+
 end
 
 Dashing.scheduler.every '5s' do
@@ -17,6 +17,7 @@ Dashing.scheduler.every '5s' do
 end
 
 Dashing.scheduler.every '30s' do
+  top_manufacturers=Device.manufacturers_dashboard
   Dashing.send_event('manufacturers', {items: top_manufacturers})
   Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
 end
@@ -25,4 +26,3 @@ end
 Dashing.scheduler.every '60s' do
   Dashing.send_event('past_day',   { current: Device.sightings_past_day })
 end
-
