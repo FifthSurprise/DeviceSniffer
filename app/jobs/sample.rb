9cdaac14cde1aa60e2b026efb5be4fcb4a0c3238
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-14 14:04:10 -0400

    updating connections

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -10,30 +10,30 @@ Dashing.scheduler.every '10s', :allow_overlapping => false do
     company = d.company !="" ? d.company : "Device Manufacturer Not Found"
     Dashing.send_event('last_MAC', { text: "#{company}",
                                      moreinfo: "MAC Address: #{d.macaddress}"})
-  end
-  ActiveRecord::Base.connection_pool.release_connection
-end
-
-Dashing.scheduler.every '5s',  :allow_overlapping => false do
-  ActiveRecord::Base.connection_pool.with_connection do
     Dashing.send_event('macaddresses',   { current: Device.total_Count })
-  end
-  ActiveRecord::Base.connection_pool.release_connection
-end
-
-Dashing.scheduler.every '30s',  :allow_overlapping => false do
-  ActiveRecord::Base.connection_pool.with_connection do
     top_manufacturers=Device.manufacturers_dashboard
     Dashing.send_event('manufacturers', {items: top_manufacturers})
     Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
-  end
-  ActiveRecord::Base.connection_pool.release_connection
-end
-
-
-Dashing.scheduler.every '60s',  :allow_overlapping => false do
-  ActiveRecord::Base.connection_pool.with_connection do
     Dashing.send_event('past_day',   { current: Device.sightings_past_day })
   end
   ActiveRecord::Base.connection_pool.release_connection
 end
+
+# Dashing.scheduler.every '5s',  :allow_overlapping => false do
+#   ActiveRecord::Base.connection_pool.with_connection do
+#   end
+#   ActiveRecord::Base.connection_pool.release_connection
+# end
+
+# Dashing.scheduler.every '30s',  :allow_overlapping => false do
+#   ActiveRecord::Base.connection_pool.with_connection do
+#   end
+#   ActiveRecord::Base.connection_pool.release_connection
+# end
+
+
+# Dashing.scheduler.every '60s',  :allow_overlapping => false do
+#   ActiveRecord::Base.connection_pool.with_connection do
+#   end
+#   ActiveRecord::Base.connection_pool.release_connection
+# end
