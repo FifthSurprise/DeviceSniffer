61084dd1941d3b232926d0bd87fea87fa6f2ccce
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-14 11:55:32 -0400

    adding back connection pool management code

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -3,29 +3,37 @@ require 'net/http'
 require 'json'
 
 Dashing.scheduler.every '10s', :allow_overlapping => false do
-  Dashing.send_event('past_hour',   { value: Device.sightings_past_hour })
+  ActiveRecord::Base.connection_pool.with_connection do
+    Dashing.send_event('past_hour',   { value: Device.sightings_past_hour })
 
-  d = Device.where("company != ''").last
-  company = d.company !="" ? d.company : "Device Manufacturer Not Found"
-  Dashing.send_event('last_MAC', { text: "#{company}",
-                                   moreinfo: "MAC Address: #{d.macaddress}"})
-ActiveRecord::Base.connection_pool.release_connection
+    d = Device.where("company != ''").last
+    company = d.company !="" ? d.company : "Device Manufacturer Not Found"
+    Dashing.send_event('last_MAC', { text: "#{company}",
+                                     moreinfo: "MAC Address: #{d.macaddress}"})
+  end
+  ActiveRecord::Base.connection_pool.release_connection
 end
 
 Dashing.scheduler.every '5s',  :allow_overlapping => false do
-  Dashing.send_event('macaddresses',   { current: Device.total_Count })
+  ActiveRecord::Base.connection_pool.with_connection do
+    Dashing.send_event('macaddresses',   { current: Device.total_Count })
+  end
   ActiveRecord::Base.connection_pool.release_connection
 end
 
 Dashing.scheduler.every '30s',  :allow_overlapping => false do
-  top_manufacturers=Device.manufacturers_dashboard
-  Dashing.send_event('manufacturers', {items: top_manufacturers})
-  Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
+  ActiveRecord::Base.connection_pool.with_connection do
+    top_manufacturers=Device.manufacturers_dashboard
+    Dashing.send_event('manufacturers', {items: top_manufacturers})
+    Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
+  end
   ActiveRecord::Base.connection_pool.release_connection
 end
 
 
 Dashing.scheduler.every '60s',  :allow_overlapping => false do
-  Dashing.send_event('past_day',   { current: Device.sightings_past_day })
+  ActiveRecord::Base.connection_pool.with_connection do
+    Dashing.send_event('past_day',   { current: Device.sightings_past_day })
+  end
   ActiveRecord::Base.connection_pool.release_connection
 end
