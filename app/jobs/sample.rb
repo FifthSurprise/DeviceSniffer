b511384fa14854ce31337997dcc7048bf886a2e2
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-14 14:45:17 -0400

    update to remove all of sent e vents

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -4,16 +4,16 @@ require 'json'
 
 Dashing.scheduler.every '10s', :allow_overlapping => false do
   ActiveRecord::Base.connection_pool.with_connection do
-    Dashing.send_event('past_hour',   { value: Device.sightings_past_hour })
+    # Dashing.send_event('past_hour',   { value: Device.sightings_past_hour })
 
-    d = Device.where("company != ''").last
-    company = d.company !="" ? d.company : "Device Manufacturer Not Found"
-    Dashing.send_event('last_MAC', { text: "#{company}",
-                                     moreinfo: "MAC Address: #{d.macaddress}"})
-    Dashing.send_event('macaddresses',   { current: Device.total_Count })
-    top_manufacturers=Device.manufacturers_dashboard
-    Dashing.send_event('manufacturers', {items: top_manufacturers})
-    Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
+    # d = Device.where("company != ''").last
+    # company = d.company !="" ? d.company : "Device Manufacturer Not Found"
+    # Dashing.send_event('last_MAC', { text: "#{company}",
+                                     # moreinfo: "MAC Address: #{d.macaddress}"})
+    # Dashing.send_event('macaddresses',   { current: Device.total_Count })
+    # top_manufacturers=Device.manufacturers_dashboard
+    # Dashing.send_event('manufacturers', {items: top_manufacturers})
+    # Dashing.send_event('kcspeed', { text: "#{Movements.last.velocity} meters/second"})
     Dashing.send_event('past_day',   { current: Device.sightings_past_day })
   end
   ActiveRecord::Base.connection_pool.release_connection
